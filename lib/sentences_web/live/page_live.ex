536cee2254cb2b8761ba36ed0866df18315745cf
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-01-21 00:23:01 +0100

    feat(take a random message when arriving on the page)

--- lib/sentences_web/live/page_live.ex
+++ lib/sentences_web/live/page_live.ex
@@ -3,51 +3,30 @@ defmodule SentencesWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{}, message: "")}
+    case Sentences.Messages.Message |> Ecto.Query.last |> Sentences.Repo.one do
+      nil -> {:ok, assign(socket, text: "", message: "Welcome, first publisher :)")}
+      last_message ->
+        message = Enum.random(1 .. last_message.id)
+        |> Sentences.Messages.get_message!()
+        {:ok, assign(socket, text: "", message: message.message)}
+    end
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("publish", %{"message" => message}, socket) do
+  def handle_event("publish", %{"text" => message}, socket) do
     case Sentences.Messages.create_message(%{"message" => message}) do
       {:ok, message} ->
-        {:noreply, redirect(socket, to: "/api/messages/#{message.id}")}
+        {:noreply,
+          socket
+          |> assign(text: "", message: message.message)
+        }
 
       _ ->
         {:noreply,
          socket
          |> put_flash(:error, "An error occured.")
-         |> assign(message: "")}
+         |> assign(message: "")
+        }
     end
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not SentencesWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
